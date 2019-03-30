75628bda77f85032b90d5c04cd84c51407b4f8b0
mfrandes <frandesmyrce@gmail.com>
2019-03-30 15:35:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">column number one</div>
+          <div className="col-6">column number one</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
