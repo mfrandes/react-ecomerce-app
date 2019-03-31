eedf79ae660c60c15e35a729902059681a888c7f
Mircea <frandesmyrce@gmail.com>
2019-03-31 21:18:01 +0300

    Creating components and inslaling react router dom

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Product from "./Product";
+export default class Navbar extends Component {
+    render() {
+        return (
+        <div>
+            <Product />
+        </div>
+        )
+    }
+}
