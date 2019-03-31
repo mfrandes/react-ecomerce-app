3adae797da46db407f4944b42893ea26dc6dfc56
Mircea <frandesmyrce@gmail.com>
2019-03-31 20:44:45 +0300

    Add css and google fonts

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-6">column number one</div>
-          <div className="col-6">column number one</div>
+          <div className="col-6">
+          <span><i className="fas fa-home" /></span></div>
         </div>
       </div>
     );
