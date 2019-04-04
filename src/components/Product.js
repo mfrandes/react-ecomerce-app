d9cc082752a2cf15d4030d2fdaf071d69ed5aa1e
Mircea <frandesmyrce@gmail.com>
2019-04-04 23:48:25 +0300

    prop types for product

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
+import PropTypes from 'prop-types';
 
-export default class Navbar extends Component {
+export default class Product extends Component {
     render() {
         const { id, title, img, price, inCart } = this.props.product;
         return (
@@ -34,15 +35,25 @@ export default class Navbar extends Component {
     }
 }
 
+Product.propTypes = {
+    product: PropTypes.shape({
+        id: PropTypes.number,
+        img: PropTypes.string,
+        title: PropTypes.string,
+        price: PropTypes.number,
+        inCart: PropTypes.bool
+    }).isRequired
+};
+
 const ProductWrapper = styled.div`
 .card {
     border-color: transparent;
-    transition: all 1s linear;
+    transition: all 0.4s linear;
 }
 .card-footer {
     background: transparent;
     border-top: transparent;
-    transition: all 1s linear;
+    transition: all 0.4s linear;
 }
 &:hover{
     .card{
@@ -58,7 +69,7 @@ const ProductWrapper = styled.div`
     overflow: hidden;
 }
 .card-img-top{
-    transition: all 1s linear;
+    transition: all 0.4s linear;
 }
 .img-container:hover .card-img-top{
     transform: scale(1.2);
@@ -74,7 +85,7 @@ const ProductWrapper = styled.div`
     font-size: 1.4rem;
     border-radius: 0.5rem 0 0 0;
     transform: translate(100%, 100%);
-    transition: all 1s linear;
+    transition: all 0.4s linear;
 }
 .img-container:hover .cart-btn{
     transform: translate(0, 0);
