28d4a4dd7a276c20e69e8f9c8f7532e9332aacaf
Mircea <frandesmyrce@gmail.com>
2019-04-07 00:23:43 +0300

    error fix

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,8 +15,8 @@ export default class Product extends Component {
                         <Link to="/details">
                             <img src={img} alt="product" className="card-img-top" />
                         </Link>
-                        <button className="cart-btn" disabeled={inCart ? true : false} onClick={() => console.log('added to the cart')}>
-                            {inCart ? (<p className="text-capitalize mb-0" disabeled>in cart</p>) : (<i className="fas fa-cart-plus" />)}
+                        <button className="cart-btn" disabled={inCart ? true : false} onClick={() => console.log('added to the cart')}>
+                            {inCart ? (<p className="text-capitalize mb-0" disabled>in cart</p>) : (<i className="fas fa-cart-plus" />)}
                         </button>
 
                     </div>
